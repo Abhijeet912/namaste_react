330a32e0b4730b2bad1b14f99ebf3fba23f84195
Abhijeet912 <abhijeet.anand837@gmail.com>
2024-11-28 16:14:25 +0530

    Adding routes to the front-end

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
+import About from './components/About';
 
 /*Header
  *  -Logo
@@ -57,9 +58,9 @@ const appRouter=createBrowserRouter([
 		path:"/about",
 		element:<About/>,
 	},
-])
+]);
 
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Applayout />);
+root.render(<RouterProvider router={appRouter} />);
